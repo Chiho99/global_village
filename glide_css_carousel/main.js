30f595867c529d84bb40e25aae20a8e5f6a7c0ad
Chiho Mikami <chihomikami.81@gmail.com>
2020-06-19 17:23:54 +0900

    add js

---
+++ glide_css_carousel/main.js
@@ -0,0 +1,12 @@
+  
+const config = {
+    type: 'carousel',
+    perView: 1,
+    breakpoints:  {
+        1100: {
+            perView: 1
+        }
+    }
+};
+new Glide('.glide', config).mount();
+
